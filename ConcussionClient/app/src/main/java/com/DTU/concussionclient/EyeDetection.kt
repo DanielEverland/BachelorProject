2c768f9c8f5b5d1c2ae6d2df3cc270a8f9a88fe0
s204500 <s204500@student.dtu.dk>
2023-02-15 11:30:30 +0100

    EyeDetection activity

---
+++ ConcussionClient/app/src/main/java/com/DTU/concussionclient/EyeDetection.kt
@@ -0,0 +1,11 @@
+package com.DTU.concussionclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EyeDetection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eye_detection)
+    }
+}
--- ConcussionClient/app/src/main/java/com/DTU/concussionclient/MainActivity.kt
+++ ConcussionClient/app/src/main/java/com/DTU/concussionclient/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.DTU.concussionclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEyeDetection = findViewById<Button>(R.id.button_eye_detection)
+        btnEyeDetection.setOnClickListener {
+            val intent = Intent(this@MainActivity, EyeDetection::class.java)
+            startActivity(intent)
+        }
     }
 }
