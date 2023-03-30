6d48ca6f73aa90228d311bb5cbbb6fdb247785e9
s204500 <s204500@student.dtu.dk>
2023-03-30 12:23:02 +0200

    Speech to text activity

--- ConcussionClient/app/src/main/java/com/DTU/concussionclient/MainActivity.kt
+++ ConcussionClient/app/src/main/java/com/DTU/concussionclient/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.DTU.concussionclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gotoBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gotoBtn = findViewById(R.id.SpeechToText_button)
+        gotoBtn.setOnClickListener {
+            val intent = Intent(this, SpeechToTextActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
