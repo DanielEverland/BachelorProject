7a9126a83fb14f01e834156a214034db712812f3
Daniel Everland <danieleverland@gmail.com>
2023-04-06 12:18:28 +0200

    Basic flashcard implementation

--- ConcussionClient/app/src/main/java/com/DTU/concussionclient/FrontPageTestButtonFragment.kt
+++ ConcussionClient/app/src/main/java/com/DTU/concussionclient/FrontPageTestButtonFragment.kt
@@ -1,5 +1,6 @@
 package com.DTU.concussionclient
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -50,7 +51,7 @@ class FrontPageTestButtonFragment : Fragment() {
         }
 
         root.findViewById<ImageButton>(R.id.testButtonFragmentButton).setOnClickListener {
-
+            startActivity(Intent(activity, TestActivity::class.java))
         }
 
         return root
